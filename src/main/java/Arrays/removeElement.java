27d777b49dc8a96644bcb2af524980ae6154702d
Swati Tiwari <56589754+swatitiwari5300@users.noreply.github.com>
2025-05-13 23:58:53 +0530

    Update removeElement.java

--- src/main/java/Arrays/removeElement.java
+++ src/main/java/Arrays/removeElement.java
@@ -9,7 +9,9 @@ public class removeElement {
     }
 
     public static int removeElement(int[] nums, int val) {
+        
         int count = 0;
+        
         for(int i = 0 ; i < nums.length ; i++){
             if(nums[i] != val){
                 nums[count] = nums[i];
