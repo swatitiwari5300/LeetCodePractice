5f5425866dc8106a4fc3bacd07e725ce310d1cd1
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-05-23 12:50:55 +0530

    code practice

---
+++ src/main/java/LinkedList/LoopInLL.java
@@ -0,0 +1,44 @@
+package LinkedList;
+
+public class LoopInLL {
+
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean hasLoop(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return  false;
+    }
+
+    public static void main(String[] args) {
+        // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        //creating a cycle
+        head.next.next.next.next.next = head.next; // 5 -> 2
+
+        System.out.println("Cycle detected: "+ hasLoop(head));
+    }
+}
