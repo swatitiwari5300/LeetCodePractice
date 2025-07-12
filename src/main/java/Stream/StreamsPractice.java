46901c7b890aa02bf783a4a18f5e7a19b09903fd
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-07-12 20:06:52 +0530

    Update StreamsPractice.java

--- src/main/java/Stream/StreamsPractice.java
+++ src/main/java/Stream/StreamsPractice.java
@@ -9,6 +9,8 @@ public class StreamsPractice {
         List<Integer> list = List.of(1,2,2,3,4,16,12,2);
         removeDup(list);
         findDuplicates(list);
+        secondHighest(list);
+        groupStringByLength("Swati Tiwari is a coder");
     }
 
     //Problem: Remove duplicates from a list of integers.
@@ -22,6 +24,18 @@ public class StreamsPractice {
         List<Integer> ans = new ArrayList<>();
         ans = list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream().filter(m -> m.getValue()>2).map(m -> m.getKey()).toList();
+        System.out.println(ans);
+    }
+
+    //Problem: Find the second-Highest number
+    public static void secondHighest(List<Integer> list){
+        Integer ans = list.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
+        System.out.println(ans);
+    }
+
+    //Problem: Group Strings by Their Length
+    public static void groupStringByLength(String str){
+        Map<Integer, List<String>> ans = Arrays.stream(str.split(" ")).collect(Collectors.groupingBy(s -> s.length()));
         System.out.println(ans);
     }
 }
