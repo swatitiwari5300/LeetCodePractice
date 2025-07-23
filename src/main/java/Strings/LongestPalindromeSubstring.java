0457b048364241878963850f98cb7fd421193826
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-07-23 23:10:56 +0530

    Add method to find longest palindrome substring
    Introduced findLongestPalindromeSubstring to identify the longest palindromic word in a space-separated string. Added a helper method isPalindrome and a main method for demonstration.

--- src/main/java/Strings/LongestPalindromeSubstring.java
+++ src/main/java/Strings/LongestPalindromeSubstring.java
@@ -1,4 +1,31 @@
 package Strings;
 
+import java.util.List;
+
 public class LongestPalindromeSubstring {
+
+    public static void main(String[] args) {
+        String str = "noon level rar naman";
+        findLongestPalindromeSubstring(str);
+    }
+
+    public static void findLongestPalindromeSubstring(String str){
+        String[] strArray = str.split(" ");
+        int maxLength = 0;
+        String ans = "";
+        for(String s : strArray){
+            if(isPalindrome(s)){
+                if(s.length()>maxLength){
+                    maxLength = s.length();
+                    ans = s;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static boolean isPalindrome(String str){
+        String rev = new StringBuilder(str).reverse().toString();
+        return rev.equals(str);
+    }
 }
