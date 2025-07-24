1e84b7849ccae21b31b1eef7f51d1b852ffc322e
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-07-24 11:43:35 +0530

    Add EasyStringQues utility class with string methods
    Introduces EasyStringQues.java containing methods to reverse a string, count vowels and consonants, and compress a string by character counts. Includes a main method demonstrating usage of these utilities.

---
+++ src/main/java/Strings/EasyStringQues.java
@@ -0,0 +1,52 @@
+package Strings;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class EasyStringQues {
+
+    public static void main(String[] args) {
+        reverseAString("Swati");
+        countVowelAndConsonants("SwAti");
+        compressString("aaabbc");
+    }
+
+
+    public static void reverseAString(String s){
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] strArr = s.split("");
+        for(int i = strArr.length-1 ; i >= 0 ; i--){
+            stringBuilder.append(strArr[i]);
+        }
+        System.out.println(stringBuilder.toString());
+    }
+
+    public static void countVowelAndConsonants(String str){
+        Map<String, Long> map = Arrays.stream(str.split("")).collect(Collectors.groupingBy(x -> {
+            if(isVowel(x)) return "Vowel";
+            return "Consonant";
+        }, Collectors.counting()));
+
+        System.out.println(map);
+    }
+
+    public static boolean isVowel(String str){
+        str = str.toLowerCase();
+        return str.equals("a") || str.equals("e") || str.equals("i") || str.equals("o") || str.equals("u");
+    }
+
+    //Compress a string using character counts
+    public static void compressString(String str){
+        StringBuilder stringBuilder = new StringBuilder();
+        Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().forEach(x -> {
+                    stringBuilder.append(x.getKey());
+                    stringBuilder.append(x.getValue());
+                });
+        System.out.println(stringBuilder.toString());
+    }
+}
