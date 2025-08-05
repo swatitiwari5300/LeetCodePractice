ba80de80dac1409d78b955bb767810560c753c74
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-08-05 23:29:52 +0530

    Update test2.java

--- src/main/java/test2.java
+++ src/main/java/test2.java
@@ -1,8 +1,36 @@
 public class test2 {
 
     public static void main(String[] args) {
-
+        //System.out.println("Try programiz.pro");
+        int[] a = {1,4,9};
+        int[] b = {4,8,15};
+        //System.out.println("hey");
+        testArr(a,b);
     }
 
+     static void testArr(int[] a, int[] b){
+        //System.out.println("hey");
+        int [] c = new int[a.length+b.length];
 
+        int i = 0, j = 0, k= 0;
+
+        while(i < a.length && j < b.length){
+
+            if(a[i] < b[j]){ c[k++] = a[i];  i++;}
+            else if(a[i] > b[j]){ c[k++] = b[j]; j++;}
+            else{
+                c[k++] = a[i];
+                c[k++] = b[j];
+                i++; j++;
+            }
+        }
+        while( i < a.length){ c[k++] = a[i++] ;}
+        while( j < b.length){ c[k++] = b[j++] ;}
+
+        //System.out.println("hey");
+
+        for(int n : c){
+            System.out.println(n);
+        }
+}
 }
