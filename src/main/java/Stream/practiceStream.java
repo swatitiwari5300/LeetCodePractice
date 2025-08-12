9ced4769c69d2a888aed12bb335270361262707c
swatitiwari5300 <swatitiwari5300@gmail.com>
2025-08-12 20:16:55 +0530

    Update practiceStream.java

--- src/main/java/Stream/practiceStream.java
+++ src/main/java/Stream/practiceStream.java
@@ -18,6 +18,10 @@ public class practiceStream {
 
     public static void main(String[] args) {
 
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 2, 4, 5, 1, 6, 3);
+        //findDupes(numbers);
+        List<String> words = Arrays.asList("java", "spring", "boot", "microservices");
+        upperCaseStr(words);
 
     List<Employees> employees = Arrays.asList(
             new Employees("A", 5000),
@@ -27,11 +31,11 @@ public class practiceStream {
     );
     //secondHighest(employees);
     //countWords("apples are very green in color apples");
-    List<String> words = Arrays.asList("java", "spring");
+    //List<String> words = Arrays.asList("java", "spring");
     //uniqueChars(words);
-        secondMaxSalEmp(employees);
+        //secondMaxSalEmp(employees);
         List<String> names = Arrays.asList("java", "spring", "c", "python", "go");
-        sortStrings(names);
+        //sortStrings(names);
 }
 
     // Find the second highest salary from a list of employees using Stream API
@@ -75,8 +79,18 @@ public class practiceStream {
     //From a list of integers, print all duplicate values.
     public static void findDupes(List<Integer> list){
 
-        
+        List<Integer> dupes = list.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().filter(x -> x.getValue()>1).map(x -> x.getKey()).toList();
+        System.out.println(dupes);
+    }
+
+    //Transform all strings to uppercase, skip first two, and print the rest.
+    public static void upperCaseStr(List<String> str){
+
+        str.stream().map(s -> s.toUpperCase()).skip(2).forEach(s -> System.out.println(s));
 
     }
+
+    //From a list of employees, group by department and find the average salary per department.
 
 }
